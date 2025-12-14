a980e27026a6ff9d58bc3172b7544a02047a4c75
Felipe Almeida <s.felipe.almeida@gmail.com>
2025-12-14 00:08:01 -0300

    Fix Next.js config for Cloudflare Pages

--- mvp/frontend/next.config.js
+++ mvp/frontend/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: 'out',
     images: {
         unoptimized: true,
     },
-    trailingSlash: true,
 }
 
 module.exports = nextConfig
